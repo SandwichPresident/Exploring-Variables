6db48199945a4c4434d150ad142b563dd9f14e65
SandwichPresident <shb1713@rit.edu>
2025-09-25 08:26:33 -0400

    Created New File since old one was broke

---
+++ ExploringVars/ExploringVars/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExploringVars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroFirstName = "Johnathan";
+            string heroLastName = "Gargantuan";
+            string heroFullName;
+
+            heroFullName = heroFirstName + " " + heroLastName;
+
+            Console.WriteLine("Behold, the legendary hero, " + heroFullName);
+        }
+    }
+}
+
+
